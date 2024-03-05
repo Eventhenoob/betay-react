ee26ad77051e1b8952cc6d7d6a8500c878cd87d5
Even <cbhrat50@gmail.com>
2024-03-05 20:22:04 +0530

    removed unused events

--- src/components/MessageForm/MessageForm.tsx
+++ src/components/MessageForm/MessageForm.tsx
@@ -40,7 +40,7 @@ const MessageForm = ({ position, setPosition }: Props) => {
   }, [errors]);
   return (
     <form
-      onSubmit={handleSubmit((data, e) => {
+      onSubmit={handleSubmit((data) => {
         if (
           !errors.firstName &&
           !errors.lastName &&
